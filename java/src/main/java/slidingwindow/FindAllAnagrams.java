8f394939f2143450de9798a5766944e1d194444f
Khushboo Kaur <ms.khushboo13@gmail.com>
2026-09-23 18:52:23 +0300

    Anagram

--- java/src/main/java/slidingwindow/FindAllAnagrams.java
+++ java/src/main/java/slidingwindow/FindAllAnagrams.java
@@ -1,5 +1,9 @@
 package slidingwindow;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/find-all-anagrams-in-a-string/
  * Given two strings s and p, return an array of all the start indices of p's anagrams in s.
@@ -14,11 +18,38 @@ package slidingwindow;
 public class FindAllAnagrams {
 
     static void main(String[] args){
-       String s = "cbaebabacd", p = "abc";
-        new FindAllAnagrams().findAnagram(s,p);
-    }
+        String str = "cbaebabacd", substr = "abc";
+        List<Integer> indices=new ArrayList<>();
 
-    private void findAnagram(String s, String p){
+        HashMap<Character,Integer> map=new HashMap<>();
 
+        //create frequency map.
+        for (Character ch : substr.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        HashMap<Character,Integer> seen=new HashMap<>();
+        int subStrLen=substr.length();
+
+        for(int i=0;i<str.length();i++){
+
+            char ch=str.charAt(i);
+            seen.put(ch,seen.getOrDefault(ch,0)+1);
+            System.out.println("Seen="+ch);
+
+            if(i>=subStrLen){                           //once we populate seen with the window size=substring, then slide the window and remove the left most character.
+                char outChar=str.charAt(i-subStrLen);   //3-3=0, when i is 3 which is 4th character, then remove 0th char.
+                int count=seen.get(outChar);
+
+                if(count==1)seen.remove(outChar);
+                else seen.put(outChar, count-1);
+                System.out.println("Decreasing count of char="+outChar+" count="+(count-1));
+            }
+            if(i>=subStrLen-1 && map.equals(seen)){      //when i=2 and subStrlen=3, from now onwards, the comparison should happen in every iteration.
+                indices.add(i-subStrLen+1);             //add charAt[2-3+1] = charAt[0] if first three chars are anagram.
+            }
+        }
+
+        System.out.println("All indices="+indices);
     }
 }
